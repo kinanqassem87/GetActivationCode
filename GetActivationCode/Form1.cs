4c1b0cd817a890c671ccb5477c74356959547e1a
kinanqassem87 <35800866+kinanqassem87@users.noreply.github.com>
2018-02-28 15:02:06 +0200

    Initial commit

---
+++ GetActivationCode/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace GetActivationCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //We need add refrence from project addReference or from reference in solution
+            //in .net tab (system.managment)
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+            ManagementObjectCollection moc = mos.Get();
+            string motherBoard = "";
+            foreach (ManagementObject mo in moc)
+            {
+                motherBoard = (string)mo["SerialNumber"];
+            }
+            textBox1.Text = motherBoard;
+        }
+    }
+}
